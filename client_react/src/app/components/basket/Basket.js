78e5fe38b22648afa916da7b8937f4e52f6f99ab
m97419 <m3197419@gmail.com>
2024-04-08 15:06:22 +0300

    add credit card dialog

--- client_react/src/app/components/basket/Basket.js
+++ client_react/src/app/components/basket/Basket.js
@@ -5,6 +5,8 @@ import { DataView } from 'primereact/dataview';
 import { classNames } from 'primereact/utils';
 import { InputNumber } from 'primereact/inputnumber';
 import BasketItem from './BasketItem';
+import { Dialog } from 'primereact/dialog';
+import { InputText } from 'primereact/inputtext';
 
 export default function Basket() {
     // const [sortKey, setSortKey] = useState('');
@@ -25,6 +27,7 @@ export default function Basket() {
 
     const [basket, setBasket] = useState(getBasket())
     const [empty, setEmpty] = useState(basket==[])
+    const [orderVisible, setOrderVisible] = useState(false)
 
     // const onSortChange = (event) => {
     //     const value = event.value;
@@ -66,8 +69,56 @@ export default function Basket() {
     return (
         <div className="card"><br/><br/>
             <DataView value={basket} listTemplate={listTemplate}/> <br/>
-            <Button icon="pi pi-credit-card" disabled={empty} raised aria-label="Filter" onClick={order}>&nbsp; to paying </Button>
+            <Button icon="pi pi-credit-card" disabled={empty} raised aria-label="Filter" onClick={()=>setOrderVisible(true)}>&nbsp; to paying </Button>
             {/* header={header()} sortField={sortField} sortOrder={sortOrder} /> */}
+            <Dialog
+            className='w-3'
+                visible={orderVisible}
+                modal
+                onHide={() => setOrderVisible(false)}
+                content={({ hide }) => (
+                    <div className="flex flex-column px-8 py-5 gap-4" style={{ borderRadius: '12px', backgroundImage: 'radial-gradient(circle at left top, var(--primary-400), var(--primary-700))' }}>
+                        <div className='text-3xl text-white'>credit-card details</div>
+                        <div className="inline-flex flex-column gap-2">
+                            <label htmlFor="number" className="text-primary-50 font-semibold">
+                                Number
+                            </label>
+                            <InputText keyfilter="pnum" id="number" label="number" className="bg-white-alpha-20 border-none p-3 text-primary-50"></InputText>
+                        </div>
+                        <div className="inline-flex flex-column gap-2">
+                            <label htmlFor="name" className="text-primary-50 font-semibold">
+                                Name
+                            </label>
+                            <InputText keyfilter="alpha" id="name" label="name" className="bg-white-alpha-20 border-none p-3 text-primary-50"></InputText>
+                        </div>
+                        <div className='flex-row'>
+                        <div className="inline-flex flex-column gap-2">
+                        <div className="inline-flex flex-column gap-2">
+                            <label className="text-primary-50 font-semibold">
+                            validity
+                            </label>
+                            <div className='flex-row'>
+                                {/* לאפשר רק 2 ספרות */}
+                            <InputText keyfilter="pnum" className="bg-white-alpha-20 border-none p-1 text-primary-50 w-2 text-xl"></InputText>&nbsp;
+                            <span className='text-3xl text-white'>/</span>&nbsp;
+                            <InputText keyfilter="pnum" className="bg-white-alpha-20 border-none p-1 text-primary-50 w-2 text-xl"></InputText>&nbsp;</div>
+                        </div>
+                            <label className="text-primary-50 font-semibold">
+                                svv
+                            </label>
+                            <div className='flex-row'>
+                                {/* לאפשר רק ספרה אחת */}
+                            <InputText keyfilter="pnum"  className="bg-white-alpha-20 border-none p-1 text-primary-50 w-1 text-xl" type="password"></InputText>&nbsp;
+                            <InputText keyfilter="pnum"  className="bg-white-alpha-20 border-none p-1 text-primary-50 w-1 text-xl" type="password"></InputText>&nbsp;
+                            <InputText keyfilter="pnum" className="bg-white-alpha-20 border-none p-1 text-primary-50 w-1 text-xl" type="password"></InputText>&nbsp;</div>
+                        </div></div>
+                        <div className="flex align-items-center gap-2">
+                            <Button label="For-Payment" onClick={(e) => {hide(e); order()}} text className="p-3 w-full text-primary-50 border-1 border-white-alpha-30 hover:bg-white-alpha-10"></Button>
+                            <Button label="Cancel" onClick={(e) => hide(e)} text className="p-3 w-full text-primary-50 border-1 border-white-alpha-30 hover:bg-white-alpha-10"></Button>
+                        </div>
+                    </div>
+                )}
+            ></Dialog>
         </div>
     )
 }
